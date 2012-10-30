c300e711b08a7590b3abaa7023533cdd5d0eb051
Héctor Valverde <hvalverde@uma.es>
2012-10-30 15:38:04 +0100

    Finished but not well documented

--- lib/Bio/Align/Subset.pm
+++ lib/Bio/Align/Subset.pm
@@ -1,9 +1,13 @@
 package Bio::Align::Subset;
 
+
 use 5.006;
 use strict;
+no strict "refs";
 use warnings;
 use Carp;
+use Bio::SeqIO;
+
 
 =head1 NAME
 
@@ -26,40 +30,28 @@ alignment.
 
 Usage example:
     
+    use strict;
+    use warnings;
+    use Data::Dumper;
+    
     use Bio::Align::Subset;
     
-    # Align file name
-    my $inAlignFile = "align.fasta";
-    # Load the file content in a file handle (FH)
-    open(FH, $inAlignFile);
-    # Align data
-    my @alignData = <FH>;
+    # The alignment in a file
+    my $filename = "alignmentfile.fas";
+    # The format
+    my $format = "fasta";
     
-    # Build the codon sets
-    my @set1 = ();
-    my @set2 = ();
-    my @set3 = ();
+    # The subset of codons
+    my $subset = [1,12,25,34,65,100,153,156,157,158,159,160,200,201,202,285];
     
-    # Store the set references
-    my $setRef1 = \@set1;
-    my $setRef2 = \@set2;
-    my $setRef3 = \@set3;
+    # Create the object
+    my $obj = Bio::Align::Subset->new(
+                                      inputfile => $filename,
+                                      format => $format
+                                    );
     
-    # Construct a new object
-    my $subAlign = Bio::Align::Subset->new();
-    # Pass set references by arguments
-    $subAlign->subsetsReferences = [$setRef1, $setRef2, $setRef3];
-    # Pass the original alignment data
-    $subAlign->originalAlignFH = @alignData;
-    # Generates the new alignments as strings
-    my @subSets = $subAlign->launch();
-    
-    # Store each subset in a file
-    for(my $i=0;$i<=$#subSets;$i++){
-        open("FOUT".$i,">file".$i.".fasta");
-        print FOUT, $subSets[$i];
-        fclose(FOUT);
-    }
+    # View the result
+    print Dumper($obj->build_subset($subset));
 
 =cut
 
@@ -73,26 +65,41 @@ Usage example:
 =head2 Bio::Align::Subset->new
 
 =cut
-#
-# The constructor of the class
-#
-sub new {
-    
-    my ($class, %arg) = @_;
-    my $self = bless {
-        _inputfile => $arg{inputfile} || croak("No file specified"),
-        _subsets   => $arg{subsets} || croak("No subsets defined")
-    }, $class;
-    $class->_incr_count();
-    return $self;
-    
-}
 
 ###############################################################################
-# Tracking methods
+# Class data and methods
 ###############################################################################
-{
+{  
+    # A list of all attributes wiht default values and read/write/required properties
+    my %_attribute_properties = (
+        _inputfile => ["????", "read.required"],
+        _format    => ["????", "read.required"],
+        _identifiers   => ["????", "read.write"   ],
+        _sequences => ["????", "read.write"   ],
+        _seq_length=> [0     , "read.write"   ]
+    );
+    
+    # Global variable to keep count of existing objects
     my $_count = 0;
+    
+    # The list of all attributes
+    sub _all_attributes {
+        keys %_attribute_properties;
+    }
+    
+    # Check if a given property is set for a given attribute
+    sub _permissions{
+        my ($self,$attribute, $permissions) = @_;
+        $_attribute_properties{$attribute}[1] =~/$permissions/;
+    }
+    
+    # Return the default value for a given attribute
+    sub _attribute_default{
+        my ($self,$attribute) = @_;
+        $_attribute_properties{$attribute}[0];
+    }
+    
+    # Manage the count of existing objects
     sub get_count{
         $_count;
     }
@@ -102,21 +109,117 @@ sub new {
     sub _decr_count{
         --$_count;
     }
+    
 }
-###############################################################################
+#
+# The constructor of the class
+#
+sub new {
+    
+    my ($class, %arg) = @_;
+    my $self = bless {}, $class;
+    
+    foreach my $attribute ($self->_all_attributes()){
+        
+        # E.g. attribute = "_name", argument = "name"
+        my ($argument) = ($attribute =~ /^_(.*)/);
+        
+        # If explicitly given
+        if(exists $arg{$argument}){
+            $self->{$attribute} = $arg{$argument};
+        }
+        
+        # If not given but required
+        elsif($self->_permissions($attribute, 'required')){
+            croak("No $argument attribute as required");
+        }
+        
+        # Set to default
+        else{
+            $self->{$attribute} = $self->_attribute_default($attribute);            
+        }
+        
+    }
+    
+    # Called $class because it is a gobal method
+    $class->_incr_count;
+    
+    $self->_extract_sequences;
+    return $self;
+    
+}
+
+
+#
+# Obtaining the sequences in a Array
+#
+sub _extract_sequences{
+    
+    my $self = $_[0];
+        
+    my @identifiers;
+    my @sequences;
+    
+    my $seqIO = Bio::SeqIO->new(
+                             -file   => $self->get_inputfile,
+                             -format => $self->get_format
+                            );
+    
+    while( my $seq = $seqIO->next_seq){
+        
+        my $sequence_string = $seq->seq;
+        $sequence_string =~ s/\s//g;
+        
+        push(@identifiers, $seq->id);
+        $self->_verify_chain($sequence_string);
+        push(@sequences, $sequence_string);
+        
+    }
+    
+    $self->set_identifiers(\@identifiers);
+    $self->set_sequences(\@sequences);
+    
+}
+
+#
+# Build a subset
+#
+sub build_subset{
+    
+    my ($self, $subset) = @_;
+    my %new_alignment = ();
+    
+    
+    # Initialite array for the new sequences
+    my @new_sequences = ();
+    
+    for(my $i=0;$i<=$#{$self->get_sequences};$i++){
+        # Initialite a new string for the new sequence
+        my $new_sequence = "";
+        for my $index (@{$subset}){
+            $new_sequence.= substr(${$self->get_sequences}[$i],$index,3);
+        }
+        push(@new_sequences, $new_sequence);
+    }
+    
+    # Build the complete hash
+    $new_alignment{sequences} = \@new_sequences;
+    $new_alignment{headers}   = $self->get_identifiers;
+    
+    return \%new_alignment;
+    
+}
 
 ###############################################################################
 # Auxiliary methods
 ###############################################################################
 {
-    
-    my $_sequence_length = 0;
-    
     #
     # Set the sequence length of the whole alignment
     #
     sub _set_sequence_length{
-        $_sequence_length = $_[0];
+        my $self = $_[0];
+        $self->{_seq_length} = $_[1];
     }
     
     #
@@ -124,8 +227,9 @@ sub new {
     # the whole alignment.
     #
     sub _check_sequence_length{
-        my $tested_sequence_length = $_[0];
-        $tested_sequence_length == $_sequence_length ? return 1 : return 0;
+        my $self = $_[0];
+        my $tested_sequence_length = $_[1];
+        $tested_sequence_length == $self->get_seq_length ? return 1 : return 0;
     }
     
     #
@@ -133,23 +237,22 @@ sub new {
     #
     sub _verify_chain{
         
-        my $sequence = $_[0];
+        my ($self,$sequence) = @_;
         my $seq_length = length($sequence);
         
+        
         # 1. The chain must be a DNA sequence
-        _isdna($sequence) ? 1 : croak("The sequence is not a dna sequence");
+        $self->_isdna($sequence) ? 1 : carp("\nWARNING: The following sequence does not seems as a dna/rna (ATGCU) sequence:\n\n<< $sequence >>\n");
         
-        # 2. Also, the input file must be wrapped (non untermitated codons)
-        $seq_length % 3 != 0 ? 1 : croak("The sequence length is not multiple of 3");
-        
-        # 3. Finally, all the sequences must be equal. But if $_sequence_length
+        # 2. Also, all the sequences must be equal. But if $_sequence_length
         # has not been updated, it takes the value of the length of this sequence.
-        if($_sequence_length == 0){
-            _set_sequence_length($seq_length);
+        if($self->get_seq_length == 0){
+            # The input file must be wrapped (non untermitated codons)
+            $seq_length % 3 == 0 ? 1 : croak("The sequence length is not multiple of 3 ($seq_length)");
+            $self->_set_sequence_length($seq_length);
         }else{
-            _check_sequence_length($seq_length) ? 1 : croak("A sequence length does not match with le length of the whole alignment");
+            $self->_check_sequence_length($seq_length) ? 1 : croak("A sequence length does not match with the length of the whole alignment");
         }
-        
         return 1;
         
     }
@@ -158,11 +261,13 @@ sub new {
     # Verifies if a given string is a DNA sequence
     #
     sub _isdna{
-        
-        my $sequence = uc($_[0]);
-        $sequence =~/^[ACGT]+$/i ? return 1: return 0;
+        my ($self,$sequence) = ($_[0],uc($_[1]));
+        if($sequence =~ /^[ACGTU]+$/){
+             return 1;
+        }else{
+             return 0;
+        }
     }
-    
     
     
 }
@@ -177,8 +282,11 @@ sub new {
 # and avoid the direct acces to the inner
 # value of $obj->{_inputfile}.
 ###############################################################################
-sub get_inputfile { $_[0] -> {_inputfile} } 
-sub get_subsets { $_[0] -> {_subsets} }
+sub get_inputfile { $_[0] -> {_inputfile} }
+sub get_format    { $_[0] -> {_format}    }
+sub get_sequences { $_[0] -> {_sequences} }
+sub get_identifiers   { $_[0] -> {_identifiers}   }
+sub get_seq_length{ $_[0] -> {_seq_length}}
 ###############################################################################
 
 
@@ -188,8 +296,14 @@ sub get_subsets { $_[0] -> {_subsets} }
 sub set_inputfile { my ($self, $inputfile) = @_;
                     $self-> {_inputfile} = $inputfile if $inputfile;
                   }
-sub set_subsets   { my ($self, $subsets) = @_;
-                    $self-> {_subsets} = $subsets if $subsets;
+sub set_format    { my ($self, $format) = @_;
+                    $self-> {_format} = $format if $format;
+                  }
+sub set_identifiers   { my ($self, $identifiers) = @_;
+                    $self-> {_identifiers} = $identifiers if $identifiers;
+                  }
+sub set_sequences { my ($self, $sequences) = @_;
+                    $self-> {_sequences} = $sequences if $sequences;
                   }
 ###############################################################################
 
